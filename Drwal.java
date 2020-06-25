a3b7e97bf616f100f3dbfdc9969ebc71fcacf6ed
lzolkiewski <62542051+lzolkiewski@users.noreply.github.com>
2020-06-25 08:48:45 +0200

    Add files via upload

--- Drwal.java
+++ Drwal.java
@@ -11,11 +11,9 @@ public class Drwal {
     private int width;
     private int height;
     String[][]loaded_array;
-    private int file_height;
-    private int file_width;
 
     public static void main(String[]args) throws IOException {
-        Drwal drwal = new Drwal(args);
+        new Drwal(args);
     }
 
     public Drwal(String[]args) throws IOException {
@@ -74,8 +72,8 @@ public class Drwal {
             if(element.length()>size)
                 size=element.length();
         }
-        file_height=file.size();
-        file_width=size;
+        int file_height = file.size();
+        int file_width = size;
 //        initialize file array
         loaded_array = new String[file.size()][size];
         for (int i = 0; i < file_height; i++) {
@@ -110,7 +108,20 @@ public class Drwal {
     }
 //    paint the place
     public void paint(){
-        // TODO: 24.06.2020 add traversing the table and painting the cells
+//        going to the highest point (at least trying)
+        get_top_corner();
+//        if floor has holes going down and painting else just paint last line
+        while (true){
+            if(check_floor()){
+                int x = xStart, y = yStart;
+                go_down();
+                fill(x, y);
+            }else {
+                fill(xStart, yStart);
+                break;
+            }
+        }
+//        displaying the painting
         display();
     }
 //    display painted part
@@ -122,9 +133,13 @@ public class Drwal {
             System.out.println();
         }
     }
-//    check celling / floor for holes
+//    check celling for holes
     public boolean check_celling(){
-        for (int i=xStart; i<width; i++){
+        int i=xStart;
+//        moving to the left to check whole celling
+        while (move_left(i, yStart)) i--;
+//        checking celling
+        for (; i<width; i++){
 //            if move right possible check above
             if (move_right(i, yStart))
 //                if move upwards possible return true as celling has hole
@@ -134,28 +149,89 @@ public class Drwal {
 //        if reached the celling has no holes
         return false;
     }
+//    check floor for holes
     public boolean check_floor(){
-        for(int i = xStart; i < width; i++){
-//            if possible to move right check underneath
-            if(move_right(i, yStart))
-//                if move down possible floor has holes
+        int i = xStart;
+//        move to the left
+        while (move_left(i, yStart))i--;
+//        check floor
+        for (;i<width; i++){
+//            if move right possible
+            if (move_right(i, yStart))
+//                if move down possible return true
                 if (move_down(i, yStart))
                     return true;
         }
-//        if reached floor has no holes
         return false;
     }
 //    check if move up / down / left / right is possible
     public boolean move_up(int x, int y){
-        return loaded_array[y - 1][x].compareTo(" ") == 0 && y > 0;
+        if (y>0)
+            return loaded_array[y - 1][x].compareTo(" ") == 0 && y > 0;
+        else
+            return false;
     }
     public boolean move_down(int x, int y){
-        return loaded_array[y + 1][x].compareTo(" ") == 0 && y < height;
+        if(y+1<height)
+            return loaded_array[y + 1][x].compareTo(" ") == 0 && y < height;
+        else
+            return false;
     }
     public boolean move_left(int x, int y){
-        return loaded_array[y][x+1].compareTo(" ")==0 && x < width;
+        if(x>0)
+            return loaded_array[y][x-1].compareTo(" ")==0 && x > 0;
+        else
+            return false;
     }
     public boolean move_right(int x, int y){
-        return loaded_array[y][x-1].compareTo(" ")==0 && x > 0;
+        if(x+1<width)
+            return loaded_array[y][x+1].compareTo(" ")==0 && x < width;
+        else
+            return false;
     }
+//    get corners
+    public void move_to_left_corner(){
+        while (move_up(xStart, yStart) || move_left(xStart - 1, yStart)) {
+            if (move_up(xStart, yStart)) yStart--;
+            else if (move_left(xStart, yStart)){
+                xStart--;
+            }
+        }
+//        needed to correct the place
+        xStart--;
+    }
+    public void move_to_right_corner(){
+        while (move_up(xStart, yStart)||move_right(xStart+1, yStart)){
+            if(move_up(xStart, yStart))yStart--;
+            else if(move_right(xStart, yStart)){
+                xStart++;
+            }
+        }
+        xStart++;
+    }
+//    supposedly going to top left corner in the marked place
+    public void get_top_corner(){
+        while (check_celling()){
+            move_to_left_corner();
+            move_to_right_corner();
+        }
+        move_to_left_corner();
+    }
+//    going down if floor has holes
+    public void go_down(){
+        if(check_floor()){
+//            if not a hole move right if possible
+            while (!move_down(xStart, yStart)&&move_right(xStart, yStart)) xStart++;
+//            move down
+            if(move_down(xStart, yStart)) yStart++;
+//            move left border if move left possible
+            while (move_left(xStart, yStart)) xStart--;
+        }
+
+    }
+//    filling the place given horizontally with color
+    public void fill(int x, int y){
+        for (int i = x; i<width && loaded_array[y][i].compareTo(" ")==0; i++)
+            loaded_array[y][i] = color;
+        }
 }
